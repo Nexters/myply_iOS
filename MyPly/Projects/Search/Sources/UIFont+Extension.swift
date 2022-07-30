9c8283061da964f5c88e02dfc6574227b277fefe
RoadtoS7 <kshvv975@gmail.com>
2022-07-30 15:35:01 +0900

    Search: UIFont extension 추가

---
+++ MyPly/Projects/Search/Sources/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont.swift
+//  Search
+//
+//  Created by nylah.j on 2022/07/30.
+//  Copyright © 2022 cocaine.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static let H1Semibold = UIFont(name: "Pretendard", size: 24)
+}
