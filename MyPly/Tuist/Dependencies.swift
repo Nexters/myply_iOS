9156fa7268c066c7a5ffe069f1c627f5235896f4
dam <37874238+choidam@users.noreply.github.com>
2022-07-30 15:50:54 +0900

    onBoarding 타겟 추가

--- MyPly/Tuist/Dependencies.swift
+++ MyPly/Tuist/Dependencies.swift
@@ -11,7 +11,8 @@ let dependencies = Dependencies(
     carthage: [],
     swiftPackageManager: [
         .remote(url: "https://github.com/Alamofire/Alamofire", requirement: .upToNextMajor(from: "5.0.0")),
-        .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.1"))
+        .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.1")),
+        .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "3.0.0"))
     ],
     platforms: [.iOS]
 )
