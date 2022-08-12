28c6ac85fad9639fc370c7bf03f3e785b945f688
Suhyeon Kim <57262833+RoadtoS7@users.noreply.github.com>
2022-08-12 23:28:06 +0900

    Common UI에 검색 화면에서 사용하는 에셋들 추가  (#4)
    * Gray80 색상 추가
    
    * default uilabel textcolor 적용안되는 이슈 수정
    
    - 이슈 원인 및 해결방안
    stored property가 아니라 computed property로 해야한다.
    
    * commonUI에 search 모듈에서 사용하는 리소스 코드 추가
    
    * commonUI에 색상 에셋 카탈로그 추가

--- MyPly/Projects/CommonUI/Sources/UIColor+MyplyColor.swift
+++ MyPly/Projects/CommonUI/Sources/UIColor+MyplyColor.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
+extension UIFont {
+    static let H1Semibold = UIFont(name: "Pretendard", size: 24)
+}
+
+
 extension UIColor {
     static let textColor = UIColor.darkGray
-    static var gray80: UIColor? { return UIColor(named: "gray80") }
+    class var begie: UIColor? { return .init(named: "begie") }
+    class var gray50: UIColor? { return .init(named: "gray50") }
+    class var gray70: UIColor? { return .init(named: "gray70") }
+    class var gray80: UIColor? { return .init(named: "gray80") }
+    class var gray90: UIColor? { return .init(named: "gray90") }
+    
+    class var greenLight: UIColor? { return .init(named: "greenLight") }
+    class var yellow: UIColor? { return .init(named: "yellow") }
+    class var red: UIColor? { return .init(named: "red") }
+    class var blue: UIColor? { return .init(named: "blue") }
+}
+
+
+extension UIImage {
+    class var search: UIImage? { return .init(named: "search")}
 }
