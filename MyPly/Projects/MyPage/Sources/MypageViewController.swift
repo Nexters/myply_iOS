daa064b48e6a8b19cc12563a6ef9786dd9251f2e
RoadtoS7 <kshvv975@gmail.com>
2022-08-12 23:47:28 +0900

    MyPageViewController titleLabel과 CollectionView를 storyboard에 배치

--- MyPly/Projects/MyPage/Sources/MypageViewController.swift
+++ MyPly/Projects/MyPage/Sources/MypageViewController.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 open class MyPageViewController: UIViewController {
+    private var titleLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+    
+    
     
     open override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension MyPageViewController {
+    static func create() -> MyPageViewController? {
+        let storyBoard = UIStoryboard(name: "MyPage", bundle: .init(for: self))
+        return storyBoard.instantiateViewController(withIdentifier: "MyPageViewController") as? MyPageViewController
+    }
+}
+// MARK: View
+extension MyPageViewController {
+    private func initUI() {
+        
     }
 }
