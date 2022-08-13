d5a3c9311aaaffa0ba49277f49954872b9bfb2ef
dam <37874238+choidam@users.noreply.github.com>
2022-08-13 14:35:27 +0900

    SignUp 추가

---
+++ MyPly/Projects/SignUp/Sources/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  SignUp
+//
+//  Created by 최모지 on 2022/08/13.
+//  Copyright © 2022 cocaine.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class SignUpViewController: UIViewController {
+    
+    open override func viewDidLoad() {
+        view.backgroundColor = .yellow
+    }
+    
+}
