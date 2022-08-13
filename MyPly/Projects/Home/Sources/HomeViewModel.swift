a893f996d6792e41d737dd416f27d41808282b5c
dochoi <dochoi@student.42seoul.kr>
2022-08-13 17:48:12 +0900

    add model module, gray30

--- MyPly/Projects/Home/Sources/HomeViewModel.swift
+++ MyPly/Projects/Home/Sources/HomeViewModel.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import Combine
+
+protocol HomePlaylistPresentable {
+
+}
+
+struct MockHomePlaylist: HomePlaylistPresentable {
+
+}
+
+protocol HomeMenu {
+
+    var title: String { get }
+    var repository: HomeRepository { get }
+}
+
+protocol HomeRepository {
+    func loadPlaylist() -> AnyPublisher<HomePlaylistPresentable, Error>
+}
+
+struct RealHomeRepository: HomeRepository {
+    func loadPlaylist() -> AnyPublisher<HomePlaylistPresentable, Error> {
+        return Future({ promise in
+            promise(.success(MockHomePlaylist()))
+        }) .eraseToAnyPublisher()
+    }
+}
+
+struct RecentlyHomeMenu: HomeMenu {
+    var repository: HomeRepository = RealHomeRepository()
+    let title: String = "최신"
+
+}
+
+struct PopularHomeMenu: HomeMenu {
+    var repository: HomeRepository = RealHomeRepository()
+    let title: String = "인기"
+}
+
+struct FavoriteHomeMenu: HomeMenu {
+    var repository: HomeRepository =  RealHomeRepository()
+
+    let title: String = "내 취향"
+}
+
+open class HomeViewModel {
+    let menus: [HomeMenu]
+
+    var currentMenu = CurrentValueSubject<HomeMenu?, Never>(nil)
+
+    init(menus: [HomeMenu]) {
+        self.menus = menus
+        currentMenu.send(menus.first)
+    }
+}
