4a4fd0ce1fc359ee7a54a9c42605bd7e89582511
RoadtoS7 <kshvv975@gmail.com>
2022-08-20 13:51:13 +0900

    serviceInfoCell 구현

--- MyPly/Projects/MyPage/Sources/ServiceInfoCell.swift
+++ MyPly/Projects/MyPage/Sources/ServiceInfoCell.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ServiceInfoCell: UICollectionViewCell {
 
+    @IBOutlet weak var titleLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func setTitle(_ text: String) {
+        titleLabel.text = text
+    }
 }
