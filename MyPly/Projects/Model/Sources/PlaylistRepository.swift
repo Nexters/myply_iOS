2527662527f7de8b018229eb3d8626f41c72b12b
RoadtoS7 <kshvv975@gmail.com>
2022-08-24 10:29:26 +0900

    DummyPlaylistRepository 접근제어자를 public으로 수정

--- MyPly/Projects/Model/Sources/PlaylistRepository.swift
+++ MyPly/Projects/Model/Sources/PlaylistRepository.swift
@@ -11,6 +11,8 @@ public protocol PlaylistRepository {
 }
 
 public class DummyPlaylistRepositoryImpl : PlaylistRepository {
+    public init() {}
+    
     public func search(param: String) async throws -> [Playlist] {
         return [
             .init(isMemoed: false, thumbnailURL: "", title: "첫번째 플레이리스트", videoDeepLink: "deep link", youtubeTags: ["tag1", "tag2"], youtubeVideoID: "viedo id")
