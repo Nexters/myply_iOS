49d5673249e4b9ca949a18d79054aa00d50fcafa
dam <37874238+choidam@users.noreply.github.com>
2022-11-06 17:28:49 +0900

    보관함 UI 오류 수정

--- MyPly/Projects/Library/Sources/ViewControllers/LibraryViewController.swift
+++ MyPly/Projects/Library/Sources/ViewControllers/LibraryViewController.swift
@@ -102,7 +102,8 @@ extension LibraryViewController {
         
         collectionView.snp.makeConstraints {
             $0.top.equalTo(headerView.snp.bottom)
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
