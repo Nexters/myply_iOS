2385d2b0e7f0a4c74d9aadcf3c89109d97a74413
dam <37874238+choidam@users.noreply.github.com>
2022-11-06 15:29:19 +0900

    토큰 여부 판별 추가

--- MyPly/Projects/App/Sources/Main/MainTabBarController.swift
+++ MyPly/Projects/App/Sources/Main/MainTabBarController.swift
@@ -7,8 +7,32 @@
 
 import Foundation
 import UIKit
+import Home
+import Search
+import Library
+import MyPage
 
 
 final class MainTabBarController: UITabBarController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.tintColor = .darkGray
+        view.backgroundColor = .white
+
+        let homeVC = HomeViewController.create()
+        homeVC?.tabBarItem = UITabBarItem(title: nil, image: AppAsset.home.image, selectedImage: nil)
+
+        let searchVC = SearchViewController()
+        searchVC.tabBarItem = UITabBarItem(title: nil, image: AppAsset.search.image, selectedImage: nil)
+
+        let libraryVC = LibraryViewController.create()
+        libraryVC?.tabBarItem = UITabBarItem(title: nil, image: AppAsset.keep.image, selectedImage: nil)
+
+        let myPageVC = MyPageViewController.create()
+        myPageVC.tabBarItem = UITabBarItem(title: nil, image: AppAsset.myPage.image, selectedImage: nil)
+
+        setViewControllers([homeVC, searchVC, libraryVC, myPageVC].compactMap { $0 }, animated: false)
+    }
 }
